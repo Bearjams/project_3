698c0242e271127485f84520559e87b4c69f5cdb
bearjam <sam.croce@gmail.com>
2020-09-21 19:54:01 -0400

    deploy

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/project3dls',
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
