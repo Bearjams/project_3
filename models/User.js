ad3b64e77ac753e4e5e9f21defde9966a0e1490c
bearjam <sam.croce@gmail.com>
2020-09-28 08:13:24 -0400

    code cleanup

--- models/User.js
+++ models/User.js
@@ -5,19 +5,9 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
     username: {
         type: String,
-        // trim: true,
-        // required: "Username is Required"
       },
       password: {
         type: String,
-        // trim: true,
-        // required: "Password is Required",
-        // validate: [
-        //   function(input) {
-        //     return input.length >= 6;
-        //   },
-        //   "Password should be longer."
-        // ]
       },
 
 });
