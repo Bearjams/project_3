358d24d69052239da94d651c0ce1ba5d6b320edd
bearjam <sam.croce@gmail.com>
2020-09-28 07:57:20 -0400

    code cleanup

--- client/src/components/AddBookBtn.js
+++ client/src/components/AddBookBtn.js
@@ -30,41 +30,6 @@ function AddBookBtn(props) {
 
 
 
-// class AddBookBtn extends Component  {
 
-// state = {
-//     savedBooks: [],
-// }
-
-// componentDidMount() {
-//     API.savedBooks()
-//         .then(savedBooks => this.setState({ savedBooks: savedBooks }))
-//         .catch(err => console.error(err));
-// }
-
-// handleSave = book => {
-
-//     if (this.state.savedBooks.map(book => book._id).includes(book._id)) {
-//         API.deleteBook(book._id)
-//             .then(deletedBook => this.setState({ savedBooks: this.state.savedBooks.filter(book => book._id !== deletedBook._id) }))
-//             .catch(err => console.error(err));
-//     } else {
-//         API.saveBook(book)
-//             .then(savedBook => this.setState({ savedBooks: this.state.savedBooks.concat([savedBook]) }))
-//             .catch(err => console.error(err));
-
-
-//     }
-// }
-// render() {
-//     return (
-//         <button o className="btn badge-pill btn-outline-warning mt-3 ml-3" >
-
-//     </button>
-//     )
-
-
-//     }
-// }
 
 export default AddBookBtn;
