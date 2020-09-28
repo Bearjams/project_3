e6bd425714cddd13d3618a3cb7c89560fb9be37c
bearjam <sam.croce@gmail.com>
2020-09-28 08:09:58 -0400

    delete unsed code

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,26 @@ import React, { useState, Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Container, Button, Alert, Modal, ModalHeader, ModalBody, ModalFooter, Label, Input } from 'reactstrap';
 import Home from "./pages/Home";
-// import Search from "./pages/search";
 import loggedIn from "./pages/loggedIn";
 import bookclub from "./pages/bookclub";
 import withAuth from "./withAuth.js";
 import './fonts.css';
-// import logged from "./components/pages/loggedIn";
+
 
 function App() {
   return (
     <Router>
       <div>
-       <Switch>
-      
-        <Route exact path="/" component={Home} />
-        
-        <Route exact path="/loggedIn" component={loggedIn} />
-      
-        {/* <Route exact path="/search" component={Search} /> */}
-        <Route exact path="/bookclub" component={bookclub} />
-        
-        {/* <Route exact path="/bookclub" component={} /> */}
+        <Switch>
+
+          <Route exact path="/" component={Home} />
+
+          <Route exact path="/loggedIn" component={loggedIn} />
+
+          <Route exact path="/bookclub" component={bookclub} />
+
         </Switch>
-      </div> 
+      </div>
     </Router>
   );
 }
